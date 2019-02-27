516ee64b0e10533cf243c2364b0a54039d1140b3
Jamie Pham <jtkp66@gmail.com>
2019-02-27 00:08:17 -0800

    finished passport auth flow.

--- services/passport.js
+++ services/passport.js
@@ -3,13 +3,36 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-const User = mongoose.model('users');
+const User = mongoose.model('users'); // User is the model class
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save(); // google user profile id. Must use save to persist to the db
-})
+passport.serializeUser((user, done) => {
+    done(null, user.id); // not profile.id. shortcut to mongodb _id
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => { // user from user model in database
+        done(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    // we already have a record with the given profile ID
+                    done(null, existingUser);
+                } else {
+                    // we don't have a user record with this ID, make a new record
+                    new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+                }
+            })
+    })
 );
