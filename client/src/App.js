99c3f8b68de45a0d6f6644d38330d3e0cb6dcf72
Jamie Pham <jtkp66@gmail.com>
2019-02-27 02:43:50 -0800

    refactored passport logic to async/await syntax. setup proxy for react dev environment

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,11 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            
+            href="/auth/google"
+            
           >
-            Learn React
+            Sign in with google
           </a>
         </header>
       </div>
--- services/passport.js
+++ services/passport.js
@@ -16,25 +16,25 @@ passport.deserializeUser((id, done) => {
         });
 });
 
-passport.use(new GoogleStrategy(
-    {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-    },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    // we already have a record with the given profile ID
-                    done(null, existingUser);
-                } else {
-                    // we don't have a user record with this ID, make a new record
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
-    })
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback",
+            proxy: true
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+
+            if (existingUser) {
+                // we already have a record with a given profile ID
+                done(null, existingUser);
+            } else {
+                // we dont have a user record with this ID, make a new record
+                const user = await new User({ googleId: profile.id }).save();
+                done(null, user);
+            }
+        }
+    )
 );
