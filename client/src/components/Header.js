60c9a473666b22fffa1f9d9f4c3598d720a2c275
Jamie Pham <jtkp66@gmail.com>
2019-03-02 01:06:22 -0800

    Accessing state in Header component and displaying content.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return(
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return <li><a href="/">Logout</a></li>;
+    }
+  }
+
   render() {
+    console.log(this.props);
     return (
-        <nav>
+      <nav>
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo">Logo</a>
           <ul id="nav-mobile" className="right">
-            <li><a href="/">Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +30,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
